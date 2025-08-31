a5b59619d7954ca7b873e5707a251dff1852a0a7
Görkem Gözen <117775308+gorkemgozen@users.noreply.github.com>
2025-09-01 00:31:12 +0300

    proje yapısındaki problemi çözdüm,

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
-const path = require("path");
-const app = express();
+const path = require('path');
+const app = express(); 
+const port = 80; 
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs'); 
 app.set("views", path.join(__dirname, "_app", "views"));
-
-app.use(express.static(path.join(__dirname, "_app", "immutable")));
+app.use(express.static(path.join(__dirname, '_app', 'immutable')));
 
 const homeRoute = require("./_app/views/routes/home");
 const aboutRoute = require("./_app/views/routes/about");
-const contactRoute = require("./_app/views/routes/contact");
+const contactRoute = require("./_app/views/routes/contact"); 
 
-app.use("/", homeRoute);
-app.use("/hakkimda", aboutRoute);
-app.use("/iletisim", contactRoute);
+app.use("/", homeRoute); app.use("/hakkimda", aboutRoute); 
+app.use("/iletisim", contactRoute); 
 
-module.exports = app;
+app.listen(port, () => { 
+    console.log("Sunucu localhost:${port} adresinde çalışıyor."); 
+});
